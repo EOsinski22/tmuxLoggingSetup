1f581996f873a4e10749c7d744386eb2e78239f5
EOsinski22 <43998695+EOsinski22@users.noreply.github.com>
2022-04-17 23:12:03 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ trap 'LAST_COMMAND=$CURRENT_COMMAND; CURRENT_COMMAND=$BASH_COMMAND' DEBUG
 #   on error: print the failed command
 trap 'ERROR_CODE=$?; FAILED_COMMAND=$LAST_COMMAND; tput setaf 1; echo "ERROR: command \"$FAILED_COMMAND\" failed with exit code $ERROR_CODE"; put sgr0;' ERR INT TERM
 
+# Check to make sure all steps within the installer completed
 FILE=$HOME/.tmux/plugins/tmux-logging/scripts/variables.sh
 if [ -f "$FILE" ]; then
     echo -e "$FILE exists."
@@ -15,9 +16,12 @@ else
     echo -e "[[ERROR]]\n$FILE does not exist.\n\nMake sure you've run the step1 script AND sourced the tmux plugins by pressing ('Prefix_key + I') from within a Tmux session\n"
 fi
 
+# QoL changes to keybinds and output files
 sed -i 's/M-p/k/g' $HOME/.tmux/plugins/tmux-logging/scripts/variables.sh
 sed -i 's/M-P/l/g' $HOME/.tmux/plugins/tmux-logging/scripts/variables.sh
 sed -i 's/M-c/L/g' $HOME/.tmux/plugins/tmux-logging/scripts/variables.sh
 sed -i 's/HOME/HOME\/tmuxlogs/g' $HOME/.tmux/plugins/tmux-logging/scripts/variables.sh
 sed -i 's/%Y%m%dT%H%M%S/%Y%m%d--%H-%M-%S/g' $HOME/.tmux/plugins/tmux-logging/scripts/variables.sh
+
+#Final steps
 echo -e "\nSetup Complete!\n\nLast Step. In a Tmux Session press: 'Prefix_key + I'\n\nCongrats. Now press 'Prefix_key + l' to capture full pane history to ~/tmuxlogs/"
